16c0a2592b426aecb08c71e1e393b0d9c614fcfb
Boris Fokin <befokin@mail.ru>
2020-06-11 17:51:21 +0300

    task_02 completed

---
+++ lesson_02 task_02/script.js
@@ -0,0 +1,21 @@
+// let num = 266219;
+
+let num = 266219;
+
+let res = 1;
+
+num = num + '';
+
+for (let i = 0;  i < num.length; i++) {
+console.log(num[ i]);
+
+ res *= num[i];
+}
+console.log(res);
+
+let result = (res ** 3);
+console.log(result);
+
+result = result + '';
+
+console.log(result.substring(0, 2));
