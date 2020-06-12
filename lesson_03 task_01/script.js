d4873566ab0d0acd8f633e4ace1466dc6beb81fe
Boris Fokin <befokin@mail.ru>
2020-06-12 23:08:59 +0300

    task_01 completed++

--- lesson_03 task_01/script.js
+++ lesson_03 task_01/script.js
@@ -1,34 +1,26 @@
 'use strict';
 
-let money = 150000;
-let income = 'Фриланс';
-let addExpenses = 'Инткрнет, такст, коммуналка';
-let deposit = true;
 let mission = 2000000;
-let period = 12;
-
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
-
-console.log(addExpenses.length);
-
-console.log("Период равен " + period + " месяцев");
+console.log(typeof mission);
 console.log("Цель заработать " + mission + " рублей");
 
+let money = +prompt('Ваш месячный доход?');
+console.log(typeof money);
+
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?');
+console.log(typeof addExpenses);
 console.log(addExpenses.toLowerCase().split(", "));
 
-money = +prompt('Ваш месячный доход?');
-
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?');
-
-deposit = confirm('Есть ли у вас депозит в банке?');
+let deposit = confirm('Есть ли у вас депозит в банке?');
+console.log(typeof deposit);
 
 let expenses1 = prompt('Введите обязательную статью расходов');
+console.log(typeof expenses1);
 
 let expenses2 = prompt('Введите обязательную статью расходов');
 
 let amount1 = +prompt('Во сколько это обойдется?');
+console.log(typeof amount1);
 
 let amount2 = +prompt('Во сколько это обойдется?');
 
