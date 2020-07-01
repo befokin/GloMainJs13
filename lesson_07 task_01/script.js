3ce3082aaa1c771aa0544fbfa9dbaa36f813a5ad
Boris Fokin <befokin@mail.ru>
2020-07-01 16:24:43 +0300

    C:/Program Files/Git/3

--- lesson_07 task_01/script.js
+++ lesson_07 task_01/script.js
@@ -49,50 +49,47 @@ let appData = {
                                     }
                                     while (!isNumber(answer2));
                                     answer2 = +answer2;
-                                    
+
                                     appData.expenses[answer2] = +answer2;
                                     console.log(appData.expenses);
                               }     
-                  }
-      };
+      },
 
-      appData.asking();
+      getExpensesMonth: function() {
+            for (let key in appData.expenses) {
+                  appData.expensesMonth += +appData.expenses[key];
+            }
+            return ;
+      },
 
-appData.getExpensesMonth = function() {
-      for (let key in appData.expenses) {
-            appData.expensesMonth += +appData.expenses[key];
-      }
-      return ;
-};
+      getBudget: function() {
+            appData.budgetMonth = appData.budget - appData.expensesMonth;
+            appData.budgetDay = Math.floor(appData.budgetMonth / 30);
+      },
 
-appData.getBudget = function() {
-      appData.budgetMonth = appData.budget - appData.expensesMonth;
-      appData.budgetDay = Math.floor(appData.budgetMonth / 30);
-};
+      getTargetMonth: function() {
+            if (Math.ceil(appData.mission / appData.budgetMonth) <= 0) {
+                  return 'Цель не будет достигнута!';
+            } else {
+                 return Math.ceil(appData.mission / appData.budgetMonth) + ' месяцев';
+            }
+      },
 
-appData.getTargetMonth = function() {
-      let res = 0;
-      if (Math.ceil(appData.mission / appData.budgetMonth) <= 0) {
-            return 'Цель не будет достигнута!';
-      } else {
-           return Math.ceil(appData.mission / appData.budgetMonth);
-      }
-};
+      getStatusIncome: function() {
 
-appData.getStatusIncome = function() {
-
-      if (appData.budgetDay >= 1200) {
-            return ('У вас высокий уровень дохода!');
-      } else if (appData.budgetDay >= 600 && appData.budgetDay < 1200) {
-            return('У вас средний уровень дохода');
-      } else if (appData.budgetDay < 600 && appData.budgetDay >= 0) {
-            return('К сожалению у вас уровень дохода ниже среднего');
-      } else {
-            return('Что-то пошло не так');
+            if (appData.budgetDay >= 1200) {
+                  return ('У вас высокий уровень дохода!');
+            } else if (appData.budgetDay >= 600 && appData.budgetDay < 1200) {
+                  return('У вас средний уровень дохода');
+            } else if (appData.budgetDay < 600 && appData.budgetDay >= 0) {
+                  return('К сожалению у вас уровень дохода ниже среднего');
+            } else {
+                  return('Что-то пошло не так');
+            }
       }
 };
 
-
+appData.asking();
 appData.getExpensesMonth();
 appData.getBudget();
 appData.getTargetMonth();
@@ -100,9 +97,9 @@ appData.getStatusIncome();
 
 console.log('Расходы за месяц: ' + appData.expensesMonth);
 
-console.log('Цель будет достигнута за период ' + appData.getTargetMonth() + ' месяцев');
+console.log('Цель будет достигнута за период ' + appData.getTargetMonth());
 
-// console.log(appData.getStatusIncome());
+console.log(appData.getStatusIncome());
  
 for (let key in appData) {
       console.log('Наша программа включает в себя данные:    свойства :  '+ key + ' значения ' + appData[key]);
