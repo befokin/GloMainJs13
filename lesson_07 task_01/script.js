5eb1744a7e9593252f43a2ddd2c5af1b8b322910
Boris Fokin <befokin@mail.ru>
2020-07-07 19:24:15 +0300

    lesson_07 task_01 completed/

--- lesson_07 task_01/script.js
+++ lesson_07 task_01/script.js
@@ -104,4 +104,5 @@ console.log(appData.getStatusIncome());
 for (let key in appData) {
       console.log('Наша программа включает в себя данные:    свойства :  '+ key + ' значения ' + appData[key]);
 }
+//
 
