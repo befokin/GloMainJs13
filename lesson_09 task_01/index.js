d2daf1ebe2f67038fda9854aafd24bd95e95384c
Boris Fokin <befokin@mail.ru>
2020-07-11 01:35:39 +0300

    lesson_09 task_01 completed

---
+++ lesson_09 task_01/index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+let start = document.getElementById('start'),
+      plus1 = document.getElementsByTagName('button')[0],
+      plus2 = document.getElementsByTagName('button')[1];
+
+console.log(start);
+console.log(plus1);
+console.log(plus2);
+
+let checkBox = document.querySelector('#deposit-check');
+      console.log(checkBox);
+
+let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+      console.log(additionalIncomeItem);
+
+let budgetDayValue = document.querySelector('.budget_day-value'),
+      expensesMonthValue = document.querySelector('.expenses_month-value'),
+      additionalIncomeValue = document.querySelector('.additional_income-value'),
+      additionalExpensesValue = document.querySelector('.additional_expenses-value'),
+      incomePeriodValue = document.querySelector('.income_period-value'),
+      targetMonthValue = document.querySelector('.target_month-value');
+
+console.log(budgetDayValue);
+console.log(budgetDayValue);
+console.log(additionalIncomeValue);
+console.log(additionalExpensesValue);
+console.log(incomePeriodValue);
+console.log(targetMonthValue);
+
+
+let salaryTitle = document.querySelector('.salary-title'),
+      salaryAmount = document.querySelector('.salary-amount'),
+      incomeTitle = document.querySelector('.income-title'),
+      incomeAmount = document.querySelector('.income-amount'),
+      additionalIncomeTitle = document.querySelector('.additional_income-title'),
+      expensesTitleTitle = document.querySelector('.expenses-title'),
+      expensesTitle = document.querySelector('[placeholder="Наименование"]'),
+      expensesAmount = document.querySelector('[placeholder="Сумма"]'),
+      additionalExpensesTitle = document.querySelector('.additional_expenses-title'),
+      additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+      depositAmount = document.querySelector('.deposit-amount'),
+      depositPercent = document.querySelector('.deposit-percent'),
+      targetAmount = document.querySelector('.target-amount'),
+      typeRange = document.querySelector('[type="range"]'),
+      budgetMonthValue = document.querySelector('.budget_month-value'),
+      cancel = document.querySelector('#cancel');
+
+console.log(salaryTitle);
+console.log(salaryAmount);
+console.log(incomeTitle);
+console.log(incomeAmount);
+console.log(additionalIncomeTitle);
+console.log(expensesTitleTitle);
+console.log(expensesTitle);
+console.log(expensesAmount);
+console.log(additionalExpensesTitle);
+console.log(additionalExpensesItem);
+console.log(depositAmount);
+console.log(depositPercent);
+console.log(targetAmount);
+console.log(typeRange);
+console.log(budgetMonthValue);
+console.log(cancel);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
