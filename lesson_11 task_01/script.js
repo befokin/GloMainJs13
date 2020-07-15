012e92d539b5faea20a0ab42327d1f8c1a79af0c
Boris Fokin <befokin@mail.ru>
2020-07-15 23:05:44 +0300

    third

--- lesson_11 task_01/script.js
+++ lesson_11 task_01/script.js
@@ -1,21 +1,14 @@
 'use strict';
 
 let start = document.getElementById('start'),
-      plus1 = document.getElementsByTagName('button')[0],
-      plus2 = document.getElementsByTagName('button')[1];
+      incomePlus = document.getElementsByTagName('button')[0],
+      expensesPlus = document.getElementsByTagName('button')[1];
 
-console.log(start);
-console.log(plus1);
-console.log(plus2);
-
-let checkBox = document.querySelector('#deposit-check');
-      console.log(checkBox);
 
 let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
-      console.log(additionalIncomeItem);
-
+let depositCheck = document.querySelector('#deposit-check');
 let budgetDayValue = document.querySelector('.budget_day-value'),
-      expensesMonthValue = document.querySelector('.expenses_month-value'),
+      expensestMonthValue = document.querySelector('.expenses_month-value'),
       additionalIncomeValue = document.querySelector('.additional_income-value'),
       additionalExpensesValue = document.querySelector('.additional_expenses-value'),
       incomePeriodValue = document.querySelector('.income_period-value'),
